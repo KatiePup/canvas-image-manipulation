e2771adecd69921909197de8d7e7a9d372c20d0d
KatiePup <katie.c.a.baker@gmail.com>
2022-01-24 21:00:44 +0000

    matrix transformation from coordinate array

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <p>
-    <button @click="backImg()">background</button>
+    <button ref="button" @click="backImg()">background</button>
+  </p>
+  <p>
+    <button ref="button" @click="overImg()">overlay</button>
   </p>
   <p>
     <canvas ref="canvas" width="300" height="300"></canvas>
@@ -12,16 +15,17 @@ export default {
   data() {
     return {
       imageUrls: {
-        background: "https://source.unsplash.com/user/c_v_r/300x300",
+        background: "/images/John300.png",
+        shorts: "/images/shorts.png",
       },
       loadedImages: {},
+      corners: [[10,10],[196,10],[30,145]],
     };
   },
   mounted() {
     for (const [key, url] of this.imageUrls.entries()) {
       const image = new Image(300, 300);
       image.src = url;
-
       return this.loadImage(key, image);
     }
   },
@@ -33,12 +37,65 @@ export default {
     getImage(key) {
       return this.loadedImages.image[key];
     },
+    drawCircle: function(ctx, x, y, radius, fill, stroke, strokeWidth) {
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+      if (fill) {
+        ctx.fillStyle = fill;
+        ctx.fill();
+      }
+      if (stroke) {
+        ctx.lineWidth = strokeWidth;
+        ctx.strokeStyle = stroke;
+        ctx.stroke();
+      }
+    },
     backImg: function () {
       // var can = document.getElementById("myCanvas");
       var can = this.$refs.canvas;
       var ctx = can.getContext("2d");
-      console.log(this.loadedImages);
-      ctx.drawImage(this.getImage("background"), 0, 0);
+
+      var img = new Image(300, 300);
+      img.src = "/images/John-O.svg";
+
+      ctx.drawImage(img, 0, 0);
+    },
+    arrayFromCoords: function (coords,xi,yi){
+      
+      let xo = coords[0][0]
+      let yo = coords[0][1]
+
+      let a = ( coords[1][0] - xo ) / xi
+      let b = ( coords[1][1] - yo ) / xi
+
+      let c = ( coords[2][0] - yo ) / yi
+      let d = ( coords[2][1] - yo ) / yi
+
+      return [[xo,yo],[a,b,c,d]]
+    },
+    overImg: function () {
+      var can = this.$refs.canvas;
+      var ctx = can.getContext("2d");
+
+      var img = new Image(196, 145);
+      img.src = "/images/shorts.png";
+
+
+      ctx.strokeStyle = "blue";
+
+      //    [ a c ] -> x1 = ax + cy, y1 = by + dx
+      //    [ b d ] -> (a, b, c, d)
+
+      var arr = this.arrayFromCoords(this.corners,196,145)
+      let [[x, y],[a,b,c,d]] = arr
+
+      ctx.setTransform(a,b,c,d,0,0);
+      
+      ctx.strokeRect(x,y, 196, 145);
+      ctx.drawImage(img, x,y);
+      
+      ctx.resetTransform();
+      this.drawCircle(ctx, 196, 145, 10, 'blue','black',1);
     },
   },
 };
