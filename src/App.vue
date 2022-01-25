b288eb23d60221241e3b7b6fd9674a15841c9f46
KatiePup <katie.c.a.baker@gmail.com>
2022-01-25 22:15:18 +0000

    ✨✨✨✨✨✨

--- src/App.vue
+++ src/App.vue
@@ -19,15 +19,24 @@ export default {
         shorts: "/images/shorts.png",
       },
       loadedImages: {},
-      corners: [[10,10],[196,10],[30,145]],
+      corners: [
+        [0, 0],
+        [150, 0],
+        [0, 150],
+      ],
     };
   },
   mounted() {
-    for (const [key, url] of this.imageUrls.entries()) {
-      const image = new Image(300, 300);
-      image.src = url;
-      return this.loadImage(key, image);
-    }
+    // for (const [key, url] of this.imageUrls.entries()) {
+    //   const image = new Image(300, 300);
+    //   image.src = url;
+    //   return this.loadImage(key, image);
+    // }
+
+    // Somehow do this programatically from css - https://stackoverflow.com/questions/2588181/canvas-is-stretched-when-using-css-but-normal-with-width-height-properties
+    var can = this.$refs.canvas;
+    can.width = 300;
+    can.height = 300;
   },
   methods: {
     loadImage(key, image) {
@@ -37,7 +46,7 @@ export default {
     getImage(key) {
       return this.loadedImages.image[key];
     },
-    drawCircle: function(ctx, x, y, radius, fill, stroke, strokeWidth) {
+    drawCircle: function (ctx, x, y, radius, fill, stroke, strokeWidth) {
       ctx.beginPath();
       ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
       if (fill) {
@@ -60,18 +69,20 @@ export default {
 
       ctx.drawImage(img, 0, 0);
     },
-    arrayFromCoords: function (coords,xi,yi){
-      
-      let xo = coords[0][0]
-      let yo = coords[0][1]
+    arrayFromCoords: function (coords, xi, yi) {
+      let xo = coords[0][0];
+      let yo = coords[0][1];
 
-      let a = ( coords[1][0] - xo ) / xi
-      let b = ( coords[1][1] - yo ) / xi
+      let a = (coords[1][0] - xo) / xi;
+      let b = (coords[1][1] - yo) / xi;
 
-      let c = ( coords[2][0] - yo ) / yi
-      let d = ( coords[2][1] - yo ) / yi
+      let c = (coords[2][0] - yo) / yi;
+      let d = (coords[2][1] - yo) / yi;
 
-      return [[xo,yo],[a,b,c,d]]
+      return [
+        [xo, yo],
+        [a, b, c, d],
+      ];
     },
     overImg: function () {
       var can = this.$refs.canvas;
@@ -80,22 +91,21 @@ export default {
       var img = new Image(196, 145);
       img.src = "/images/shorts.png";
 
-
       ctx.strokeStyle = "blue";
 
       //    [ a c ] -> x1 = ax + cy, y1 = by + dx
       //    [ b d ] -> (a, b, c, d)
 
-      var arr = this.arrayFromCoords(this.corners,196,145)
-      let [[x, y],[a,b,c,d]] = arr
+      var arr = this.arrayFromCoords(this.corners, 196, 145);
+      let [[x, y], [a, b, c, d]] = arr;
 
-      ctx.setTransform(a,b,c,d,0,0);
+      ctx.setTransform(a, b, c, d, 0, 0);
       
-      ctx.strokeRect(x,y, 196, 145);
-      ctx.drawImage(img, x,y);
+      ctx.strokeRect(x, y, 196, 145);
+      ctx.drawImage(img, x, y);
       
       ctx.resetTransform();
-      this.drawCircle(ctx, 196, 145, 10, 'blue','black',1);
+      this.drawCircle(ctx, 196, 145, 10, "blue", "black", 1);
     },
   },
 };
